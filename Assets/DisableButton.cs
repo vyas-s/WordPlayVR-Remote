6b2d3f8fd9efe193649331c2d55884e4a1e1caf8
Vyas <vyasvarun95@gmail.com>
2018-11-24 01:45:35 -0800

    Final touches and major changes to UI.

---
+++ Assets/DisableButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisableButton : MonoBehaviour {
+
+    public Button playButton;
+    Image playImg;
+
+    // Use this for initialization
+    void Start () {
+        playImg = gameObject.GetComponent<Image>();//.color = new Color32(255, 255, 225, 100);
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (playButton.IsInteractable())
+        {
+            playImg.color = new Color32(50, 218, 134, 255);
+        }
+        else
+        {
+            playImg.color = new Color32(155, 155, 155, 255);
+        }
+	}
+}
